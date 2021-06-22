9200a993eaa1e38e5b0deb5f77adf0fc753c8d21
Anatolii Zarytskyi <tolia.zaritskiy@gmail.com>
2021-06-22 15:18:46 +0300

    added solution

---
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -0,0 +1,29 @@
+package taxi.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    public static final String URL = "jdbc:mysql://localhost:3306/hw_db?"
+            + "useUnicode=true&serverTimezone=UTC&useSSL=false";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "101351013";
+    public static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    static {
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't find SQL Driver", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        Properties dbProperties = new Properties();
+        dbProperties.setProperty("user", USERNAME);
+        dbProperties.setProperty("password", PASSWORD);
+        return DriverManager.getConnection(URL, dbProperties);
+    }
+}
